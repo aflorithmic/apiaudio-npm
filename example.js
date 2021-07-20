36bfc330024523310b3e7739fa6e7ccdf3515443
zeritte <zeritte@gmail.com>
2021-07-20 13:16:03 +0100

    fix examplejs

--- example.js
+++ example.js
@@ -4,17 +4,16 @@ require("dotenv").config();
 async function example() {
   try {
     apiaudio.configure({
-      apiKey: process.env.API_KEY
+      apiKey: process.env.API_KEY,
+      bearer: "eyJdsadsdfdj... (pass one of the apiKey or bearer)",
+      debug: true
     });
-    let script = await apiaudio.Script.create({
-      scriptText:
-        "Hello world this is an amazing day wuhu I am in London but quarantineee no matter no worries everything will be okay and this is my day 4 I am trying to create loooong mastering to see if it is going to charge me correctly, meaning duration based. anyways london is a cool place if you are alone and doing quarantine its a bit boring but it will be fun and I plan to go to barcelona soon because I have got friends there and I wanna swim in barcelona beachs. that is it for now lets see how much this will take.Hello world this is an amazing day wuhu I am in London but quarantineee no matter no worries everything will be okay and this is my day 4 I am trying to create loooong mastering to see if it is going to charge me correctly, meaning duration based. anyways london is a cool place if you are alone and doing quarantine its a bit boring but Hello world this is an amazing day wuhu I am in London but quarantineee no matter no worries everything will be okay and this is my day 4 I am trying to create loooong mastering to see if it is going to charge me correctly, meaning duration based. anyways london is a cool place if you are alone and doing quarantine its a bit boring but it will be fun and I plan to go to barcelona soon because I have got friends there and I wanna swim in barcelona beachs. that is it for now lets see how much this will take.Hello world this is an amazing day wuhu I am in London but quarantineee no matter no worries everything will be okay and this is my day 4 I am trying to create loooong mastering to see if it is going to charge me correctly, meaning duration based. anyways london is a cool place if you are alone and doing quarantine its a bit boring but Hello world this is an amazing day wuhu I am in London but quarantineee no matter no worries everything will be okay and this is my day 4 I am trying to create loooong mastering to see if it is going to charge me correctly, meaning duration based. anyways london is a cool place if you are alone and doing quarantine its a bit boring but it will be fun and I plan to go to barcelona soon because I have got friends there and I wanna swim in barcelona beachs. that is it for now lets see how much this will take.Hello world this is an amazing day wuhu I am in London but quarantineee no matter no worries everything will be okay and this is my day 4 I am trying to create loooong mastering to see if it is going to charge me correctly, meaning duration based. anyways london is a cool place if you are alone and doing quarantine its a bit boring but",
-      scriptName: "minuteLong11"
-    });
+
+    let script = await apiaudio.Script.create({ scriptText: "hello world!" });
     console.log(script);
 
-    // script = await apiaudio.Script.retrieve(script["scriptId"]);
-    // console.log(script);
+    script = await apiaudio.Script.retrieve(script["scriptId"]);
+    console.log(script);
 
     let speech = await apiaudio.Speech.create({
       scriptId: script["scriptId"],
@@ -30,8 +29,8 @@ async function example() {
     });
     console.log(mastering);
 
-    // let masteringResult = await apiaudio.Mastering.retrieve(script["scriptId"]);
-    // console.log(masteringResult);
+    let masteringResult = await apiaudio.Mastering.retrieve(script["scriptId"]);
+    console.log(masteringResult);
   } catch (e) {
     console.error(e);
   }
