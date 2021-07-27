2842bbeb0e61b07ba69578ee826774cb4a273617
a96lex <a96lex@gmail.com>
2021-07-27 14:15:58 +0200

    add parameters and filtering functions

--- src/Sound.ts
+++ src/Sound.ts
@@ -1,7 +1,7 @@
 import { apiaudio } from "./apiaudio";
 import { isInitializedError, isSubmoduleAlreadyInitializedError } from "./Errors";
 import { RequestBase } from "./RequestBase";
-import { IConfig, ISoundBody } from "./types";
+import { IConfig, ISoundBody, ISoundTemplatesFilteringBody } from "./types";
 
 export class SoundClass {
   #initialized = false;
@@ -10,6 +10,8 @@ export class SoundClass {
   #file_url = "";
   #bg_url = "";
   #template_url = "";
+  #template_url_v2 = "";
+  #parameters_url = "";
 
   public configure(config: IConfig, requestClass: RequestBase): void {
     if (this.#initialized) {
@@ -19,6 +21,8 @@ export class SoundClass {
     this.#file_url = `${config.baseUrl}/file/sound`;
     this.#bg_url = `${config.baseUrl}/file/background_track`;
     this.#template_url = `${config.baseUrl}/file/soundtemplates`;
+    this.#template_url_v2 = `${config.baseUrl}/sound/list`;
+    this.#parameters_url = `${config.baseUrl}/sound/parameters`;
     this.#initialized = true;
     this.#RequestClass = requestClass;
   }
@@ -66,6 +70,26 @@ export class SoundClass {
     return this.#RequestClass.getRequest(this.#template_url);
   }
 
+  /**
+   * List all the available sound templates (newer version, allows optional filtering)
+   */
+  public list_v2(params?: ISoundTemplatesFilteringBody): Promise<unknown> {
+    if (!this.#initialized) {
+      isInitializedError();
+    }
+    return this.#RequestClass.getRequest(this.#template_url_v2, "", { params });
+  }
+
+  /**
+   * List all allowed filtering parameters
+   */
+  public parameters(): Promise<unknown> {
+    if (!this.#initialized) {
+      isInitializedError();
+    }
+    return this.#RequestClass.getRequest(this.#parameters_url);
+  }
+
   public reset(): void {
     this.#initialized = false;
     // @ts-ignore
@@ -74,6 +98,8 @@ export class SoundClass {
     this.#file_url = "";
     this.#bg_url = "";
     this.#template_url = "";
+    this.#template_url_v2 = "";
+    this.#parameters_url = "";
   }
 }
 
