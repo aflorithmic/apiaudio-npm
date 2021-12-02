5bb09416a39ef41e9562f0a96aee9bd203948598
zeritte <zeritte@gmail.com>
2021-12-02 12:55:10 +0000

    add in birdcache resource

---
+++ src/Birdcache.ts
@@ -0,0 +1,40 @@
+import { apiaudio } from "./apiaudio";
+import { isInitializedError, isSubmoduleAlreadyInitializedError } from "./Errors";
+import { RequestBase } from "./RequestBase";
+import { IConfig, IBirdcacheBody } from "./types";
+
+export class BirdcacheClass {
+  #initialized = false;
+  #RequestClass!: RequestBase;
+  #url = "";
+
+  public configure(config: IConfig, requestClass: RequestBase): void {
+    if (this.#initialized) {
+      isSubmoduleAlreadyInitializedError();
+    }
+    this.#url = `${config.baseUrl}/birdcache`;
+    this.#initialized = true;
+    this.#RequestClass = requestClass;
+  }
+
+  /**
+   * create a single production request with birdcache
+   * @param data
+   */
+  public create(data: IBirdcacheBody): Promise<unknown> {
+    if (!this.#initialized) {
+      isInitializedError();
+    }
+    return this.#RequestClass.postRequest(this.#url, data);
+  }
+
+  public reset(): void {
+    this.#initialized = false;
+    // @ts-ignore
+    this.#RequestClass = undefined;
+    this.#url = "";
+  }
+}
+
+export const Birdcache = new BirdcacheClass();
+apiaudio.register(Birdcache);
