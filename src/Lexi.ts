7110cef655fcaa1d8dc0afbe20f7e02e6c11de51
zeritte <zeritte@gmail.com>
2022-08-18 11:39:09 +0100

    implement methods

--- src/Lexi.ts
+++ src/Lexi.ts
@@ -1,7 +1,7 @@
 import { apiaudio } from "./apiaudio";
 import { isInitializedError, isSubmoduleAlreadyInitializedError } from "./Errors";
 import { RequestBase } from "./RequestBase";
-import { IConfig } from "./types";
+import { IConfig, IDictionaryRegisterCustomWordBody } from "./types";
 
 export class LexiClass {
   #initialized = false;
@@ -10,6 +10,8 @@ export class LexiClass {
   #words_url = "";
   #types_url = "";
   #search_url = "";
+  #custom_dict_path = "";
+  #custom_word_path = "";
 
   public configure(config: IConfig, requestClass: RequestBase): void {
     if (this.#initialized) {
@@ -19,6 +21,8 @@ export class LexiClass {
     this.#words_url = `${config.baseUrl}/diction/list`;
     this.#types_url = `${config.baseUrl}/diction/list_types`;
     this.#search_url = `${config.baseUrl}/diction/search`;
+    this.#custom_dict_path = `${config.baseUrl}/diction/custom`;
+    this.#custom_word_path = `${config.baseUrl}/diction/custom/item`;
     this.#initialized = true;
     this.#RequestClass = requestClass;
   }
@@ -63,6 +67,56 @@ export class LexiClass {
       isInitializedError();
     }
     return this.#RequestClass.getRequest(this.#search_url, `${word}/${language}`);
+  }
+
+  /**
+   * List words in a custom dictionary
+   */
+  public customWords(lang: string): Promise<unknown> {
+    if (!this.#initialized) {
+      isInitializedError();
+    }
+    return this.#RequestClass.getRequest(this.#custom_word_path, "", {
+      params: { lang }
+    });
+  }
+
+  /**
+   * List of custom dictionaries
+   */
+  public customDicts(): Promise<unknown> {
+    if (!this.#initialized) {
+      isInitializedError();
+    }
+    return this.#RequestClass.getRequest(this.#custom_dict_path);
+  }
+
+  /**
+   * register a word to a custom dictionary
+   * @param data
+   */
+  public registerCustomWord(data: IDictionaryRegisterCustomWordBody): Promise<unknown> {
+    if (!this.#initialized) {
+      isInitializedError();
+    }
+    if (!data.specialization) data.specialization = "default";
+    if (!data.contentType) data.contentType = "basic";
+    return this.#RequestClass.putRequest(this.#custom_word_path, data);
+  }
+
+  /**
+   * Delete a custom word
+   * @param word required
+   * @param lang required
+   * @param specialization optional
+   */
+  public deleteCustomWord(word: string, lang: string, specialization?: string): Promise<unknown> {
+    if (!this.#initialized) {
+      isInitializedError();
+    }
+    return this.#RequestClass.deleteRequest(this.#custom_word_path, "", {
+      params: { word, lang, specialization }
+    });
   }
 
   public reset(): void {
