9c02995fcb594114e12af4101dfcbc9e2e70e41c
zeritte <zeritte@gmail.com>
2022-08-23 11:49:57 +0100

    get rid of types and words functions in lexi

--- src/Lexi.ts
+++ src/Lexi.ts
@@ -7,8 +7,6 @@ export class LexiClass {
   #initialized = false;
   #RequestClass!: RequestBase;
   #list_url = "";
-  #words_url = "";
-  #types_url = "";
   #search_url = "";
   #custom_dict_path = "";
   #custom_word_path = "";
@@ -18,8 +16,6 @@ export class LexiClass {
       isSubmoduleAlreadyInitializedError();
     }
     this.#list_url = `${config.baseUrl}/diction`;
-    this.#words_url = `${config.baseUrl}/diction/list`;
-    this.#types_url = `${config.baseUrl}/diction/list_types`;
     this.#search_url = `${config.baseUrl}/diction/search`;
     this.#custom_dict_path = `${config.baseUrl}/diction/custom`;
     this.#custom_word_path = `${config.baseUrl}/diction/custom/item`;
@@ -37,26 +33,6 @@ export class LexiClass {
     return this.#RequestClass.getRequest(this.#list_url, "", {
       params: { lang, type }
     });
-  }
-
-  /**
-   * List the available types
-   */
-  public types(): Promise<unknown> {
-    if (!this.#initialized) {
-      isInitializedError();
-    }
-    return this.#RequestClass.getRequest(this.#types_url);
-  }
-
-  /**
-   * Lists all the words contained in a dictionary
-   */
-  public words(dictId: string): Promise<unknown> {
-    if (!this.#initialized) {
-      isInitializedError();
-    }
-    return this.#RequestClass.getRequest(this.#words_url, dictId);
   }
 
   /**
@@ -124,8 +100,6 @@ export class LexiClass {
     // @ts-expect-error
     this.#RequestClass = undefined;
     this.#list_url = "";
-    this.#words_url = "";
-    this.#types_url = "";
     this.#search_url = "";
   }
 }
