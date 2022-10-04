af06d1ab340a35dc99bad5a9dff6bc764fc68d4b
Tim Coggins <timcoggins@gmail.com>
2022-10-04 16:03:49 +0200

    Changed back baseUrls to v1

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://v2.api.audio";
-export const API_BASE_URL_STAGING = "https://staging-v2.api.audio";
+export const API_BASE_URL = "https://v1.api.audio";
+export const API_BASE_URL_STAGING = "https://staging-v1.api.audio";
